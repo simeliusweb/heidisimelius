3d7afdb47fe6bae58d14545e058f4e129628ced9
januzgi <janisuoranta@icloud.com>
2025-10-09 21:09:06 +0300

    Add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,20 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    sitemap({
+      hostname: "https://www.heidisimelius.fi",
+      dynamicRoutes: [
+        "/",
+        "/bio",
+        "/keikat",
+        "/galleria",
+        "/bilebandi-heidi-and-the-hot-stuff",
+      ],
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
